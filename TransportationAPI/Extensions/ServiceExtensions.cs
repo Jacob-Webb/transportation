077d20ecdc9ba6e299c9bdfad928b504d9988637
Jacob Webb <jacob@jacob-webb.com>
2021-11-18 07:44:39 -0800

    added extensions folder

---
+++ TransportationAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TransportationAPI.Data;
+
+namespace TransportationAPI.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(option =>
+            {
+                option.AddPolicy("AllowAll", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                );
+            }
+
+            );
+        }
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentityCore<ApplicationUser>(query => query.User.RequireUniqueEmail = false);
+            builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), services);
+            builder.AddEntityFrameworkStores<transportationContext>().AddDefaultTokenProviders();
+        }
+
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration Configuration)
+        {
+            var jwtSettings = Configuration.GetSection("Jwt");
+            var key = Configuration["Jwt:Key"];
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(option =>
+            {
+                option.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.GetSection("Issuer").Value,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+                };
+            });
+        }
+
+        public static void ConfigureTwilio(this IServiceCollection services, IConfiguration configuration)
+        {
+            var accountSid = configuration["Twilio:accountSid"];
+            var authToken = configuration["Twilio:authToken"];
+            TwilioClient.Init(accountSid, authToken);
+        }
+    }
+}
---
+++ TransportationAPI/Extensions/UserManagerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TransportationAPI.Data;
+
+namespace TransportationAPI.Extensions
+{
+    public static class UserManagerExtensions
+    {
+        public static async Task<ApplicationUser> FindByPhoneAsync(this UserManager<ApplicationUser> userManager, string phoneNumber)
+        {
+            return await userManager?.Users?.SingleOrDefaultAsync(user => user.PhoneNumber == phoneNumber);
+        }
+    }
+}
