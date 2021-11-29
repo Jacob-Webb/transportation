b3c1faff90fa5b6c9753bfb9abe420d2ccc54c7c
Jacob Webb <jacob@jacob-webb.com>
2021-11-29 09:36:29 -0800

    added tests for TimeOfDay.cs

---
+++ TransportationAPITests/Types/TimeOfDayTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using TransportationAPI.Types;
+
+namespace TransportationAPITests
+{
+    [TestFixture]
+    public class TimeOfDayTests
+    {
+        public TimeOfDay _timeOfDay;
+        [SetUp]
+        public void Setup()
+        { 
+        }
+
+        [Test]
+        public void Hour_AssignedNegativeValue_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _timeOfDay.Hour = -1);
+        }
+
+        [Test]
+        public void Hour_AssignedValueGreaterThan23_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _timeOfDay.Hour = 24);
+        }
+
+        [Test]
+        public void Minutes_AssignedNegativeValue_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _timeOfDay.Minutes = -1);
+        }
+
+        [Test]
+        public void Minutes_AssignedValueGreaterThan59_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _timeOfDay.Minutes = 60);
+        }
+
+    }
+}
