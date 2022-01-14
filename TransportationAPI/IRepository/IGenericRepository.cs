b310e8a4e9f58754935c75111185b04bc7159fdc
Jacob Webb <jacob@jacob-webb.com>
2022-01-14 13:35:38 -0800

    refactor: add synchronous equivalent of async methods
    some sync methods are required to be used by the program

--- TransportationAPI/IRepository/IGenericRepository.cs
+++ TransportationAPI/IRepository/IGenericRepository.cs
@@ -30,12 +30,32 @@ namespace TransportationAPI.IRepository
                    Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
 
         /// <summary>
-        /// Return an instance of from the set of type <typeparamref name="T"/> that matches the parameters.
+        /// Return all instances in the set of type <typeparamref name="T"/> that matches the parameters.
+        /// </summary>
+        /// <param name="expression">An <see cref="Expression"/> used as a query for objects of type <typeparamref name="T"/>.</param>
+        /// <param name="orderBy">A <see cref="Func{T, TResult}"/> used to order the query results.</param>
+        /// <param name="include">A <see cref="Func{T, TResult}"/> to include parameters in the query.</param>
+        /// <returns>A Generic List of type <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        IList<T> GetAll(
+                   Expression<Func<T, bool>> expression = null,
+                   Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                   Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
+
+        /// <summary>
+        /// Return an instance from the set of type <typeparamref name="T"/> that matches the parameters.
         /// </summary>
         /// <param name="expression">An <see cref="Expression"/> used as a query for objects of type <typeparamref name="T"/>.</param>
         /// <param name="include">A <see cref="Func{T, TResult}"/> to include parameters in the query.</param>
         /// <returns>An instance of <see cref="Task{TResult}"/> representing the result.</returns>
         Task<T> GetAsync(Expression<Func<T, bool>> expression, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
+
+        /// <summary>
+        /// Return an instance from the set of type <typeparamref name="T"/> that matches the parameters.
+        /// </summary>
+        /// <param name="expression">An <see cref="Expression"/> used as a query for objects of type <typeparamref name="T"/>.</param>
+        /// <param name="include">A <see cref="Func{T, TResult}"/> to include parameters in the query.</param>
+        /// <returns>An instance of <see cref="Task{TResult}"/> representing the result.</returns>
+        T Get(Expression<Func<T, bool>> expression, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
 
         /// <summary>
         /// Creates an entity of type <typeparamref name="T"/> in a repository.
@@ -45,6 +65,12 @@ namespace TransportationAPI.IRepository
         Task InsertAsync(T entity);
 
         /// <summary>
+        /// Creates an entity of type <typeparamref name="T"/> in a repository.
+        /// </summary>
+        /// <param name="entity">An instance of type <typeparamref name="T"/> to be created in the repository.</param>
+        void Insert(T entity);
+
+        /// <summary>
         /// Creates instances of an entity of type <typeparamref name="T"/> from a collection.
         /// </summary>
         /// <param name="entities">A collection of entities of type <see cref="IEnumerable{T}"/>.</param>
@@ -52,11 +78,23 @@ namespace TransportationAPI.IRepository
         Task InsertRangeAsync(IEnumerable<T> entities);
 
         /// <summary>
-        /// Removes from a repository an entity whose id matches the given parameter.
+        /// Creates instances of an entity of type <typeparamref name="T"/> from a collection.
         /// </summary>
-        /// <param name="id">An <c>int</c> representing an entity's id.</param>
+        /// <param name="entities">A collection of entities of type <see cref="IEnumerable{T}"/>.</param>
+        void InsertRange(IEnumerable<T> entities);
+
+        /// <summary>
+        /// Finds and Removes an entity with the given primary key.
+        /// </summary>
+        /// <param name="id">An <c>int</c> representing an entity's primary key.</param>
         /// <returns>An instance of type <see cref="Task"/>.</returns>
         Task DeleteAsync(int id);
+
+        /// <summary>
+        /// Finds and Removes an entity with the given primary key.
+        /// </summary>
+        /// <param name="id">An <c>int</c> representing an entity's primary key.</param>
+        void Delete(int id);
 
         /// <summary>
         /// Removes from a repository a collection of entities.
--- TransportationAPI/Repository/GenericRepository.cs
+++ TransportationAPI/Repository/GenericRepository.cs
@@ -28,6 +28,12 @@ namespace TransportationAPI.Repository
             _dbSet.Remove(entity);
         }
 
+        public void Delete(int id)
+        {
+            var entity = _dbSet.Find(id);
+            _dbSet.Remove(entity);
+        }
+
         public void DeleteRange(IEnumerable<T> entities)
         {
             _dbSet.RemoveRange(entities);
@@ -43,6 +49,18 @@ namespace TransportationAPI.Repository
             }
 
             return await query.AsNoTracking().FirstOrDefaultAsync(expression);
+        }
+
+        public T Get(Expression<Func<T, bool>> expression, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (include != null)
+            {
+                query = include(query);
+            }
+
+            return query.AsNoTracking().FirstOrDefault(expression);
         }
 
         public async Task<IList<T>> GetAllAsync(
@@ -70,14 +88,49 @@ namespace TransportationAPI.Repository
             return await query.AsNoTracking().ToListAsync();
         }
 
+        public IList<T> GetAll(
+            Expression<Func<T, bool>> expression = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (expression != null)
+            {
+                query = query.Where(expression);
+            }
+
+            if (include != null)
+            {
+                query = include(query);
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            return query.AsNoTracking().ToList();
+        }
+
         public async Task InsertAsync(T entity)
         {
             await _dbSet.AddAsync(entity);
         }
 
+        public void Insert(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
         public async Task InsertRangeAsync(IEnumerable<T> entities)
         {
             await _dbSet.AddRangeAsync(entities);
+        }
+
+        public void InsertRange(IEnumerable<T> entities)
+        {
+            _dbSet.AddRange(entities);
         }
 
         public void Update(T entity)
